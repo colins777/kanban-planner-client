573d8791b850fe096e05e3a5084ae3d4121f8a3f
colins777 <rumyga@gmail.com>
2024-05-13 22:22:58 +0300

    timer fty added on timer page

--- client/src/hooks/useLocalStorage.ts
+++ client/src/hooks/useLocalStorage.ts
@@ -11,6 +11,7 @@ export function useLocalStorage<T>({
 }: IUseLocalStorage<T>): [T, Dispatch<SetStateAction<T>>, boolean] {
 	const [isLoading, setIsLoading] = useState(true)
 
+	//to check if data from localstorage is loaded
 	const isMounted = useRef(false)
 	const [value, setValue] = useState<T>(defaultValue)
 
