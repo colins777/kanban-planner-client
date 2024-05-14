77175b705178528abbf80cae31585784f576b239
colins777 <rumyga@gmail.com>
2024-05-14 17:05:40 +0300

    added active menu styles

--- client/src/components/dashboard-layout/sidebar/MenuItem.tsx
+++ client/src/components/dashboard-layout/sidebar/MenuItem.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { IMenuItem } from './menu.interface'
+//import styles from './MenuItem.module.scss'
 
 export function MenuItem({ item }: { item: IMenuItem }) {
+
+	const pathname = usePathname()
+
+	//console.log('pathname', pathname)
+	//console.log('item', item)
+
 	return (
 		<div>
 			<Link
 				href={item.link}
-				className='flex gap-2.5 items-center py-1.5 mt-2 px-layout transition-colors hover:bg-border rounded-lg'
+				className={'flex gap-2.5 items-center py-1.5 mt-2 px-layout transition-colors hover:bg-border rounded-lg ' + (pathname == item.link ? 'text-purple-600' : '')}
 			>
 				<item.icon />
 				<span>{item.name}</span>
